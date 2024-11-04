2b3837d44329479977725b26bc99610e291c81b9
Rayze421 <michaelsschmuck@gmail.com>
2024-11-03 21:59:47 -0500

    Filled Items

--- worlds/lily's well/Items.py
+++ worlds/lily's well/Items.py
@@ -5,3 +5,50 @@ class llwItem(Item):
     game: str = "Lily's Well"
 
     
+Bad_Rope_Material = {
+    "Toilet_Paper": 2313,
+    "Red_Flower_Vine": 2314,
+    "Yellow_Flower_Vine": 2315,
+    "Charger_Wire": 2316
+}
+
+Good_Rope_Material = {
+    "Whip": 2317,
+    "Knitted_Yarn": 2318,
+    "Power_Chord": 2319,
+    "Oily_Hair": 2320,
+    "Net": 2321,
+    "Blue_Flower_Vine": 2322,
+    "Chain": 2323,
+    "Bedsheets": 2324,
+    "Belts": 2325
+}
+
+Yarn = {
+    "Ball_Of_Yarn": 2326,
+}
+
+Tools = {
+    "Knitting_Needles": 2327,
+    "Knife": 2328,
+    "Shed_Key": 2329,
+    "Garden_Shears": 2330,
+    "Red_Key": 2331,
+    "Bolt_Cutters": 2332,
+    "ID_Card": 2333,
+    "Jerkey": 2334
+}
+
+Rocks = {
+    "Rock": 2335
+}
+
+Notes = {
+    "Reciept": 2336,
+    "Plant_Book": 2337,
+    "Birthday_Card": 2338
+}
+
+Maybe = {
+    "Move_Stool": 2339
+}
