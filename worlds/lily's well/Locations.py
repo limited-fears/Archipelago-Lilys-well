f604ddd7252425f550f4471a644d589476dcc3b8
limited-fears <49963808+limited-fears@users.noreply.github.com>
2024-11-03 21:46:12 -0500

    finished location.py

--- worlds/lily's well/Locations.py
+++ worlds/lily's well/Locations.py
@@ -4,7 +4,45 @@ class LLwLocation(Location):
     game: str = "Lily's Well"
 
     locsphere0_table = {
-"bed": 450670,
-"Bedroom_Drawer": 450671,
-"":450672
+        "bed": 450670,
+        "Bedroom_Drawer": 450671,
+        "bathroom":450672,
+        "Dad's drawer":450673,
+        "Driveway":450674,
+        "Woods":450675,
+        "Well_Entrance":450676,
+        "Dad's_Coat":450677,
+        "Couch_Right":450678,
+        "Bookshelf":450679,
+        "Kitchen_Pantry":450680,
+        "Shed_Outside":450681
+        }
+
+    locsphere1_table ={
+        "Dad's_Safe":450682,
+        "Kitchen_Wall":450683,
+        "Outside_Left":450684,
+        "Shed_Right":450685,
+        "Shed_Left":450686,
+        "Cave_Note":450687,
+        "Bridge":450688,
+        "Red_Tree":450689,
+        "Blue_Tree":450690,
+        "Yellow_Tree":450691,
+        "Lab_Cage":450692
     }
+
+    locsphere2_table = {
+        "Lab_Guilt":450693,
+        "Couch_Left":450694,
+        "Altar":450695
+    }
+
+    locspere3_table = {
+        "Cave_Net":450696,
+        "Cave_Wall":450697
+}
+    locprogress_table = {
+        "yarn":55,
+        "stool":56
+}
