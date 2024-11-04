59bba979030c7811ed86e0c01774d0d82e363d31
limited-fears <49963808+limited-fears@users.noreply.github.com>
2024-11-03 21:52:18 -0500

    changed the ids

--- worlds/lily's well/Locations.py
+++ worlds/lily's well/Locations.py
@@ -4,43 +4,43 @@ class LLwLocation(Location):
     game: str = "Lily's Well"
 
     locsphere0_table = {
-        "bed": 450670,
-        "Bedroom_Drawer": 450671,
-        "bathroom":450672,
-        "Dad's drawer":450673,
-        "Driveway":450674,
-        "Woods":450675,
-        "Well_Entrance":450676,
-        "Dad's_Coat":450677,
-        "Couch_Right":450678,
-        "Bookshelf":450679,
-        "Kitchen_Pantry":450680,
-        "Shed_Outside":450681
+        "bed": 231370,
+        "Bedroom_Drawer": 231371,
+        "bathroom":231372,
+        "Dad's drawer":231373,
+        "Driveway":231374,
+        "Woods":231375,
+        "Well_Entrance":231376,
+        "Dad's_Coat":231377,
+        "Couch_Right":231378,
+        "Bookshelf":231379,
+        "Kitchen_Pantry":231380,
+        "Shed_Outside":231381
         }
 
     locsphere1_table ={
-        "Dad's_Safe":450682,
-        "Kitchen_Wall":450683,
-        "Outside_Left":450684,
-        "Shed_Right":450685,
-        "Shed_Left":450686,
-        "Cave_Note":450687,
-        "Bridge":450688,
-        "Red_Tree":450689,
-        "Blue_Tree":450690,
-        "Yellow_Tree":450691,
-        "Lab_Cage":450692
+        "Dad's_Safe":231382,
+        "Kitchen_Wall":231383,
+        "Outside_Left":231384,
+        "Shed_Right":231385,
+        "Shed_Left":231386,
+        "Cave_Note":231387,
+        "Bridge":231388,
+        "Red_Tree":231389,
+        "Blue_Tree":231390,
+        "Yellow_Tree":231391,
+        "Lab_Cage":231392
     }
 
     locsphere2_table = {
-        "Lab_Guilt":450693,
-        "Couch_Left":450694,
-        "Altar":450695
+        "Lab_Guilt":231393,
+        "Couch_Left":231394,
+        "Altar":231395
     }
 
     locspere3_table = {
-        "Cave_Net":450696,
-        "Cave_Wall":450697
+        "Cave_Net":231396,
+        "Cave_Wall":231397
 }
     locprogress_table = {
         "yarn":55,
